60ca6e1d12f0805322d174a295a5580d6bb672c0
Jhonatan Gasperi • NeuroNex AI <suporte.neuronex@gmail.com>
2026-07-26 17:51:17 -0300

    Delete unused NeuralCast export barrel

--- src/features/neuralcast/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as NeuralCastLanding } from "./pages/NeuralCastLanding";
-export { default as NeuralCastNewsletter } from "./pages/NeuralCastNewsletter";
-export * from "./content";
